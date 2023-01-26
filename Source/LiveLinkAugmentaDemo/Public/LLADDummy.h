3dbe1deb11beb9c8e2a9eea74dc323253f7c94f8
BenoitArbelot <benoit.arbelot@gmail.com>
2023-01-26 21:19:43 +0100

    Testing new AugmentaManager.

---
+++ Source/LiveLinkAugmentaDemo/Public/LLADDummy.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LIVELINKAUGMENTADEMO_API LLADDummy
+{
+public:
+	LLADDummy();
+	~LLADDummy();
+};
